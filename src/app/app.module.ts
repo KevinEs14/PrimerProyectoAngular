5b3605881ff1cb61d3e5109889f8abd5defa898c
KevinEs14 <escobarkev@gmail.com>
2020-11-03 08:14:32 -0400

    subida proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import {app_routing} from "./app.routes";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Route, RouterModule, Routes } from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+
+
+const app_routes: Routes=[
+    {path: 'home',component: HomeComponent},
+    {path: 'about',component: AboutComponent},
+    {path: '**', pathMatch:'full',redirectTo:'home'}
+];
+
+export const app_routing=RouterModule.forRoot(app_routes);
+// export const app_routing=RouterModule.forRoot(app_routes, {useHash: true});
